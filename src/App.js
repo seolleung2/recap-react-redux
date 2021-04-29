ab785529045068c046651e9774a15974947cdd4d
seolleung2 <seolleung22@gmail.com>
2021-04-29 17:49:28 +0900

    [Feat] 숫자 증가 애플리케이션 작성 (리액트 클래스형 컴포넌트) - 리액트 props, event 를 매개로 작성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import AddNumberRoot from "./Components/AddNumberRoot";
+import DisplayNumberRoot from "./Components/DisplayNumberRoot";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { number: 0 };
+  render() {
+    return (
+      <div className="App">
+        <h1>Root</h1>
+        <AddNumberRoot
+          handleClick={(size) =>
+            this.setState({ number: this.state.number + size })
+          }
+        ></AddNumberRoot>
+        <DisplayNumberRoot number={this.state.number}></DisplayNumberRoot>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/AddNumber.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+export default class AddNumber extends Component {
+  state = { size: 1 };
+  render() {
+    return (
+      <div>
+        <h1>Add Number</h1>
+        <input
+          type="button"
+          value="+"
+          onClick={() => this.props.handleClick(this.state.size)}
+        />
+        <input
+          type="text"
+          value={this.state.size}
+          onChange={(e) => this.setState({ size: Number(e.target.value) })}
+        />
+      </div>
+    );
+  }
+}
